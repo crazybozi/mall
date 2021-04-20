912f72ad72b4b1bdbe8e2bb5b0361c3a044c09e4
ffz <2693281866@qq.com>
2021-04-20 14:14:31 +0800

    封装axios

---
+++ src/utils/axios.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { Toast } from "vant";
+
+axios.defaults.baseURL =
+  process.env.NODE_ENV == "development"
+    ? "//121.43.231.185:8080/api/v1"
+    : "//121.43.231.185:8080/api/v1";
+axios.defaults.withCredentials = true;
+//封装请求的基础路径
+axios.defaults.headers["X-Requested-With"] = "XMLHttpRequest";
+//自定义请求头
+axios.defaults.headers["token"] = localStorage.getItem("token") || "";
+//统一请求头的Content-Type类型为application/json
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
+
+//axios拦截器
+axios.interceptors.response.use((res) => {
+  if (typeof res.data !== "object") {
+    Toast.fail("服务端异常！");
+    return Promise.reject(res);
+  }
+  if (res.data.resultCode != 200) {
+    if (res.data.message) Toast.fail(res.data.message);
+    if (res.data.resultCode == 416) {
+      window.vRouter.push({ path: "/login" });
+    }
+    return Promise.reject(res.data);
+  }
+  return res.data;
+});
+
+export default axios;
